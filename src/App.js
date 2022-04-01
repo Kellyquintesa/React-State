ab86b4b09e6f212d61e14f44ad8ec9fa8001c675
Kellyquintesa <kellyquintesa11@gmail.com>
2022-04-01 21:14:23 +0700

    add span

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App">Kelly fasdfsadfasdf</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Welcome to my counter app</h1>
+      <div className="value">
+        <button onClick={decrement}>-</button>
+        <p>{count} </p>
+        <button onClick={increment}>+</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
